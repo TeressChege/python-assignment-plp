07ea492bcf18f68ce997c27913ee533369dcf9fc
TeressChege <132881798+TeressChege@users.noreply.github.com>
2025-08-14 15:56:36 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
+x=input("Enter number:")
+y=input("Enter new number:")
 x=input("Enter number:")
 y=input("Enter new number:")
 multiplication=int(x) * int(y)
-=======
-x=input("Enter number:")
-y=input("Enter new number:")
-multiplication=int(x) * int(y)
->>>>>>> 2e1af3d2a2f105653528003dc6cb0dd10e3ac52b
 print("The answer is:",multiplication )
